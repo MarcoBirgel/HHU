fe56468b0012f077fdcb3104313365043c6d1751
Marco Birgel <marco.birgel@hhu.de>
2024-10-14 11:40:46 +0200

    Add files via upload

---
+++ sem1/progra/u002/Square.java
@@ -0,0 +1,11 @@
+public class Square {
+
+    public static void main (String []args) {
+
+        // Eingabe der ganzen Zahl
+        int number = Integer.parseInt(args[0]);
+
+        // Ausgabe des Quadrats
+        System.out.println(number*number);
+    }
+}
